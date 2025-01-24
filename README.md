117d8ffd65cd4acd17d436a48ef57c8e155bde4c
Abhay singh sisoodiya <abhaysingh2005xyz@gmail.com>
2025-01-24 09:32:21 +0530

    Added Outputs.
    Added output for the questions of unit 1 and unit 2.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RDS-LAB
  This repository contains the lab-activities on my R-for-data-science course at my college.
+
+
--- Unit2/Unit2_Q4.R
+++ Unit2/Unit2_Q4.R
@@ -13,7 +13,6 @@ Array <- array(data = 1:48 , dim = c(4,2,6) , dimnames = list(Rname , Cname , Ln
 Lname2 <-c("L1","L2", "L3", "L4", "L5")
 
 New_Array2 <- array(data = Array[,,-6] , dim = c(4,2,5) , dimnames = list(Rname , Cname , Lname2))
-print(New_Array2)
 
 # Solution of Q5:
 -99 -> New_Array2[c(2,4),2,c(1,3,5)]
