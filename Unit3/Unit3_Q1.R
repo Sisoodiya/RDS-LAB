c0d3fd8cac2d52a945c6fa32e550442f705eac90
Abhay singh sisoodiya <abhaysingh2005xyz@gmail.com>
2025-02-08 09:52:15 +0530

    Update Unit3_Q1.R

--- Unit3/Unit3_Q1.R
+++ Unit3/Unit3_Q1.R
@@ -7,4 +7,6 @@ I10 <- diag(10)
 # This implies if i and j represents indexes of a matrix then for (i != j) Index 
 # element equal to 0 for i = 1:10 and 1:10.
 print(length(I10[I10 == FALSE]))
-print(I10 == 0) # Evrey TRUE is the place where the element is equal to 0.
+print(I10 == 0) # Every TRUE is the place where the element is equal to 0.
+
+print(which(x = (I10 == 0), arr.ind = TRUE))
