8e1c1eccfe0ef40c6a64004d16a7f9ca81aa180b
Abhay singh sisoodiya <abhaysingh2005xyz@gmail.com>
2025-02-10 20:16:32 +0530

    Update Unit3_Q3.R

--- Unit3/Unit3_Q3.R
+++ Unit3/Unit3_Q3.R
@@ -2,3 +2,13 @@
 #      i. Use a check for equality to confirm that the sub-string beginning with character 5
 #         and ending with character 10 is "6-pack".
 #      ii. Make it a better deal by changing the price to $10.99.
+
+# Solution :
+string <- "Two 6-packs for $12.99"
+
+# i.
+print(substr(x = string, start = 5, stop = 10) == "6-pack")
+print(substring(text = string, first = 5, last = 10) == "6-pack")
+
+# ii.
+substr(x = string, start = )
