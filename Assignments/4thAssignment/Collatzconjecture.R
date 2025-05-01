ad4bc528993e5fed9c23faf6b09fba598b8a9253
Abhay singh sisoodiya <abhaysingh2005xyz@gmail.com>
2025-05-01 10:56:50 +0530

    added 25th question.
    This question is done in a nonchalant way.

---
+++ Assignments/4thAssignment/Collatzconjecture.R
@@ -0,0 +1,55 @@
+# This file will store a function named "CollatzConjecture" that will take in an user input
+# This user input will be the number we want to make the Collatz conjecture of.
+
+add_to_factor <- function(factor_vec, new_elem) {
+  new_elem <- as.character(new_elem)  # Convert to character to ensure proper level matching
+  
+  # Add new level if not present
+  if (!(new_elem %in% levels(factor_vec))) {
+    levels(factor_vec) <- c(levels(factor_vec), new_elem)
+  }
+  
+  # Append the new element (as character)
+  factor_vec <- c(factor_vec, factor(new_elem, levels = levels(factor_vec), ordered = TRUE))
+  
+  return(factor_vec)
+}
+
+CollatzConjecture <- function(input) {
+  # This function will print the collatz conjecture of the given number.
+  
+  if (input <= 0) {
+    # cat("The conjecture only works for positive integers.\n")
+    return(NULL)
+  }
+  
+  conjecture <- factor(as.character(input), levels = as.character(input), ordered = TRUE)
+  
+  # cat("Collatz Conjecture Sequence:\n")
+  # cat(input, "\n")
+  
+  if (input == 1) {
+    for (val in c(4, 2, 1)) {
+      # cat(val, "\n")
+      conjecture <- add_to_factor(conjecture, val)
+    }
+  } else {
+    while (input != 1) {
+      if (input %% 2 == 0) {
+        input <- input / 2
+      } else {
+        input <- 3 * input + 1
+      }
+      # cat(input, "\n")
+      conjecture <- add_to_factor(conjecture, input)
+    }
+  }
+  
+  # cat("\nFinal Factor:\n")
+  # print(conjecture)
+  return(conjecture)
+}
+
+# Get user input
+# input_num <- as.integer(readline("Enter the number you want to make conjecture of: "))
+# CollatzConjecture(input_num)
--- Assignments/4thAssignment/Q25.R
+++ Assignments/4thAssignment/Q25.R
@@ -1,2 +1,38 @@
 ## Q25. Write R code that dynamically adds named elements to an existing list based on 
 # user input.
+
+# Solution :-
+
+# This imports the function CollatzConjecture()
+# The bellow is the absolute path to my CollatzConjecture.R file.
+source("~/Documents/Github/RDS-LAB/Assignments/4thAssignment/Collatzconjecture.R")
+
+conjecture <- CollatzConjecture(16)
+
+example_list <- list(
+  "This is a example list.",
+  conjecture,
+  matrix(
+    data = c(1,0,0,0,1,0,0,0,1),
+    nrow = 3,
+    ncol = 3,
+    byrow = TRUE,
+    dimnames = list(c("row1", "row2", "row3"),
+         c("col1", "col2", "col3"))
+  ) %*% diag(c(2,3,5))
+)
+
+# print(example_list)
+
+for(i in 1:length(x = example_list)){
+  cat("Current element :\n")
+  print(example_list[[i]])
+  
+  name <- readline(
+    prompt = paste0("Enter a name for the element ", i , ": ")
+  )
+  names(example_list)[i] <- name
+}
+
+# Don't know why it changes the column names of the matrix
+print(example_list)
